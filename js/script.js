8da32ed391f490cc1a351e5881af164423bc4138
Ashish suman <Ashish9264suman@gmail.com>
2024-03-13 21:30:30 +0530

    basic stucture of website is done

---
+++ js/script.js
@@ -0,0 +1,27 @@
+document.getElementById('myForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    
+    const formData = new FormData(this);
+    const url = 'https://api.web3forms.com/submit';
+    const formId = 'b6cca412-5f61-4462-92b2-b0f6bc7dc0c8'
+    
+    formData.append('form_id', formId);
+    
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        body: formData
+      });
+      
+      if (response.ok) {
+        document.getElementById('statusMessage').textContent = 'Form submitted successfully!';
+        document.getElementById('myForm').reset();
+      } else {
+        document.getElementById('statusMessage').textContent = 'Form submission failed!';
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      document.getElementById('statusMessage').textContent = 'Error submitting form!';
+    }
+  });
+  
