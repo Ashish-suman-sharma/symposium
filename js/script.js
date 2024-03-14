0f64646a5a608456d7253a6690e6295fc6543cf7
Ashish suman <Ashish9264suman@gmail.com>
2024-03-14 10:55:22 +0530

    added table

--- js/script.js
+++ js/script.js
@@ -1,27 +1,42 @@
-document.getElementById('myForm').addEventListener('submit', async function(event) {
+document
+  .getElementById("myForm")
+  .addEventListener("submit", async function (event) {
     event.preventDefault();
-    
+
     const formData = new FormData(this);
-    const url = 'https://api.web3forms.com/submit';
-    const formId = 'b6cca412-5f61-4462-92b2-b0f6bc7dc0c8'
-    
-    formData.append('form_id', formId);
-    
+    const url = "https://api.web3forms.com/submit";
+    const formId = "b6cca412-5f61-4462-92b2-b0f6bc7dc0c8";
+
+    formData.append("form_id", formId);
+
     try {
       const response = await fetch(url, {
-        method: 'POST',
-        body: formData
+        method: "POST",
+        body: formData,
       });
-      
+
       if (response.ok) {
-        document.getElementById('statusMessage').textContent = 'Form submitted successfully!';
-        document.getElementById('myForm').reset();
+        document.getElementById("statusMessage").textContent =
+          "Form submitted successfully!";
+        document.getElementById("myForm").reset();
       } else {
-        document.getElementById('statusMessage').textContent = 'Form submission failed!';
+        document.getElementById("statusMessage").textContent =
+          "Form submission failed!";
       }
     } catch (error) {
-      console.error('Error submitting form:', error);
-      document.getElementById('statusMessage').textContent = 'Error submitting form!';
+      console.error("Error submitting form:", error);
+      document.getElementById("statusMessage").textContent =
+        "Error submitting form!";
     }
   });
+
+
+  // yaha se popup ka hai
   
+function openPopup() {
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
